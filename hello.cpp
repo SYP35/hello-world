5205bea7a111b3344fca23b0d47fa7e61a9c2b7a
syp <syp@syp.168.5.19>
2020-05-15 17:41:44 +0800

    syp新增hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct Foo {
+    int n;
+    
+    Foo() {
+        std::cout << "static constructor\n";
+    }
+
+    ~Foo() {
+        std::cout << "static destructor\n";
+    }
+};
+
+Foo f;
+
+int main()
+{
+    std::cout << "main function\n";
+
+}
+
